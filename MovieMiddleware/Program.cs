a1bd867ea6225cef199012d5184c170878915a90
Leapheng Keam <leaphengkeam@gmail.com>
2025-12-02 22:05:58 -0500

    installed Swagger

--- MovieMiddleware/Program.cs
+++ MovieMiddleware/Program.cs
@@ -4,8 +4,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+// Enable Swagger only in Development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
